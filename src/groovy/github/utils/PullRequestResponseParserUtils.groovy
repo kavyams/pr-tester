a9da81e864898757c18662f2f601f76ce0508614
Kavya Sampath Kumar <ksampathkumar@netflix.com>
2018-11-11 18:08:47 -0800

    include forgotten parser util file

---
+++ src/groovy/github/utils/PullRequestResponseParserUtils.groovy
@@ -0,0 +1,43 @@
+package groovy.github.utils
+
+import groovy.github.valueObjects.PullRequestObject
+import groovy.util.logging.Slf4j
+
+@Slf4j
+class PullRequestResponseParserUtils {
+
+    /**
+     * Extract fields from the server response and populate PullRequestObject
+     * @param responseFromServer
+     * @param keys to extract
+     * @return
+     */
+    public
+    static List<PullRequestObject> parseGitHubServerResponseArray(responseFromServer, keys) {
+        List<PullRequestObject> pullRequestObjects = new ArrayList<PullRequestObject>()
+        for (int i = 0; i < responseFromServer.size(); i++) {
+            PullRequestObject pullRequestObject = parseGitHubServerResponse(responseFromServer[i], keys)
+            pullRequestObjects.add(pullRequestObject)
+        }
+        return pullRequestObjects
+    }
+
+    public
+    static PullRequestObject parseGitHubServerResponse(responseFromServer, keys) {
+        PullRequestObject pullRequestObject = new PullRequestObject()
+        for (String key in keys) {
+            pullRequestObject.setProperty(key, responseFromServer.get(key))
+        }
+        return pullRequestObject
+    }
+
+    public
+    static List<String> parseSpecificKey(pullRequestObjects, keyToExtract) {
+        List<Object> values = new ArrayList<Object>()
+        for (PullRequestObject pullRequestObject in pullRequestObjects) {
+            values.add(pullRequestObject.getProperty(keyToExtract));
+        }
+        return values
+    }
+}
+
